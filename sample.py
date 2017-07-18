cde9cf30dc4f39d71356bdf35ac1191f5c418e45
root <root@localhost.localdomain>
2017-07-18 17:42:00 -0400

    Estamos en el MainFrame

--- sample.py
+++ sample.py
@@ -13,7 +13,7 @@ banner = r"""
 hackerPrint("\n[-] Welcome hacker this is hackerPrint i will show you a couple of samples you can do with this simple script ","IRHUMAN",False)
 hackerPrint("\n[-] BTW it only works on Linux XD","IRHUMAN",True)
 sleep(1)
-hackerPrint("\n\nBOOTING COMPUTER PLEASE WAIT","HEAVY",True,0.01)
+hackerPrint("\n\nBOOTING MAINFRAME COMPUTER PLEASE WAIT","HEAVY",True,0.01)
 hackerPrint("............","HEAVY",True,0.1)
 hackerPrint("DONE","GOOD",True,0.01)
 sleep(2)
@@ -26,7 +26,7 @@ hackerPrint(banner,"HEAVY",True,0.01)
 
 
 hackerPrint("\n[#] This is an Error Message","ERROR",False)
-hackerPrint("\n[xD] This is some hevy stuff","HEAVY",False)
+hackerPrint("\n[xD] This is some heavy stuff","HEAVY",False)
 hackerPrint("\n[*] Wow somthing good happend","GOOD",False)
 hackerPrint("\n[!] Crap this is bad","ERROR",True)
 hackerPrint("\n[-] This is really heavy DOC","HEAVY",True)
