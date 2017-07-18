8459411a5fc1db4b0158d21cc2618af42d993031
root <root@localhost.localdomain>
2017-07-18 17:24:05 -0400

    Here We GOOOOO

---
+++ sample.py
@@ -0,0 +1,35 @@
+from HackerPrint import hackerPrint
+from time import sleep
+
+banner = r"""
+  ___ ___                __               __________        .__        __   
+ /   |   \_____    ____ |  | __ __________\______   \_______|__| _____/  |_ 
+/    ~    \__  \ _/ ___\|  |/ // __ \_  __ \     ___/\_  __ \  |/    \   __\
+\    Y    // __ \\  \___|    <\  ___/|  | \/    |     |  | \/  |   |  \  |  
+ \___|_  /(____  /\___  >__|_ \\___  >__|  |____|     |__|  |__|___|  /__|  
+       \/      \/     \/     \/    \/                               \/      
+"""
+
+hackerPrint("\n[-] Welcome hacker this is hackerPrint i will show you a couple of samples you can do with this simple script ","IRHUMAN",False)
+hackerPrint("\n[-] BTW it only works on Linux XD","IRHUMAN",True)
+sleep(1)
+hackerPrint("\n\nBOOTING COMPUTER PLEASE WAIT","HEAVY",True,0.01)
+hackerPrint("............","HEAVY",True,0.1)
+hackerPrint("DONE","GOOD",True,0.01)
+sleep(2)
+hackerPrint("\n\nHacker@RootTerminal: ","HEAVY",True,0.01)
+sleep(1)
+hackerPrint("Execute Demo.script","IRHUMAN",True)
+sleep(1)
+
+hackerPrint(banner,"HEAVY",True,0.01)
+
+
+hackerPrint("\n[#] This is an Error Message","ERROR",False)
+hackerPrint("\n[xD] This is some hevy stuff","HEAVY",False)
+hackerPrint("\n[*] Wow somthing good happend","GOOD",False)
+hackerPrint("\n[!] Crap this is bad","ERROR",True)
+hackerPrint("\n[-] This is really heavy DOC","HEAVY",True)
+hackerPrint("\n[+] Yeaaah now we are Talking","GOOD",True)
+hackerPrint("\n[!!] RUN FOR YOUR LIFE","VERYBAD",True,0.01)
+print "\n"
